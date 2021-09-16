97ce7144baa41891d854def099d97826b0c26b73
--unset <vitaly.little@gmail.com>
2021-09-16 11:15:29 +0400

    Add okhttp3 dependency and create interceptor class.

--- app/src/main/java/com/online/booking/BaseFragment.kt
+++ app/src/main/java/com/online/booking/BaseFragment.kt
@@ -1,15 +1,24 @@
 package com.online.booking
 
 import androidx.fragment.app.Fragment
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 open class BaseFragment : Fragment() {
     public final val BASE_URL = "http://10.0.2.2:8080/"
 
     protected val retrofit : Retrofit =
         Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    protected val okHttpClient : OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout( 30, TimeUnit.SECONDS )
+            .readTimeout( 30, TimeUnit.SECONDS )
+            .writeTimeout( 30, TimeUnit.SECONDS )
+            .build()
 }
